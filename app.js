2632f0f5ac1ec7c1ea7ade17538c2d3943c89289
kushp12 <kushanp12@gmail.com>
2014-09-29 16:15:27 -0400

    Comments_and_adjusting_css

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
+	// When new item is submitted, jQuery will append the item a <li> tag//
 	$('.button').click(function(){
 		$('.items').append($('<li>', {
 			text: $('.texts').val()
 	}))
-		$('form').on('click', function(event){
-				event.preventDefault();
-				$('.texts').val('');
+	$('form').on('click', function(event){
+		event.preventDefault();
+		$('.texts').val('');
 	})
+	// If a null item is submitted, the app will alert the user to enter a proper, viable item //
 	var new_Item = $('.texts').val();
 		if (new_Item == ''){
 			alert('Please Enter a Valid Item');
@@ -17,9 +19,11 @@ $(document).ready(function(){
 			return $('li').last().remove();
 		}
 	})
+	// When a item that is already on the list is clicked, it will replace the <li> with a different class that will cross the item out//
 	$('ul').on('click', 'li', function(){
 		$(this).toggleClass('complete');
 	})
+	// This button deletes the entire list //
 	$('div').on('click', '.delete', function(){
 		$('li').hide();
 	});
